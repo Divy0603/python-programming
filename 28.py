a67df50c252d745d036e1ed35f24613eb8d3ad05
Divy0603 <100995948+Divy0603@users.noreply.github.com>
2022-06-01 18:45:42 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=int(input("enter the number of rows"))
+b=int(input("enter the number of columns"))
+l=[[]]
+for i in range(a):
+    for j in range(b):
+        p=int(input())
+        l.append([])
+print(l)
